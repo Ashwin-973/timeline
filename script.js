226a733ad0c5054486f25aa63fd6c6ff63f7128d
Tony Montana <ashwin.s1336@gmail.com>
2026-01-04 14:52:27 +0530

    feat : implement core logic for scroll progress

--- script.js
+++ script.js
@@ -1,2 +1,40 @@
 
 
+const triggers=document.querySelectorAll('.track-trigger')
+const progressBar=document.querySelector('.track-progress')
+
+const totalTriggers=triggers.length-1
+
+
+const options = {
+    rootMargin: '-45% 0px -55% 0px',  // Trigger when element is in near the center of viewport
+    threshold: 0
+}
+
+function triggerCallback(entries, observer){
+
+    entries.forEach((entry) => {
+
+        entry.target.classList.toggle('active', entry.isIntersecting);
+
+        if(entry.isIntersecting){
+            const activeIndex=entry.target.getAttribute('data-index');
+            updateState(activeIndex)
+            
+        }
+        
+    })
+
+}
+
+const updateState=(activeIndex)=>{
+    const progress= (activeIndex/totalTriggers)*100;
+    progressBar.style.height=`${progress}%`
+}
+
+
+const triggerObserver = new IntersectionObserver(triggerCallback, options)
+
+triggers.forEach((trigger)=>{
+    triggerObserver.observe(trigger)
+})
